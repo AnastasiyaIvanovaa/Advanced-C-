72c508936efe4052babc8af7d7c1db73405b93d1
Anastasiya.Ivanova <agb66666@gmail.com>
2022-03-21 12:00:57 +0200

    Added the solutions of Stacks And Queues(Exercise).

---
+++ StacksAndQueues-Exercise/06.SongsQueue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] songsInput = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> songs = new Queue<string>(songsInput);
+
+            while (songs.Count>0)
+            {
+                string command = Console.ReadLine();
+
+                if (command.Contains( "Play"))
+                {
+                    songs.Dequeue();
+                }
+                else if (command.Contains( "Add"))
+                {
+                    string nameOfTheSong = command.Substring(4);
+                    if (!songs.Contains(nameOfTheSong))
+                    {
+                        songs.Enqueue(nameOfTheSong);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{nameOfTheSong} is already contained!");
+                    }
+                }
+                else if (command.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ",songs));
+                }
+
+            }
+            Console.WriteLine("No more songs!");
+                    }
+    }
+}
---
+++ StacksAndQueues-Exercise/12.CupsAndBottles/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.CupsAndBottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> cups = new Queue<int>(cupsInput);
+            int[] bottlesInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> bottels = new Stack<int>(bottlesInput);
+            int wastedWater = 0;
+
+            while (cups.Count>0 && bottels.Count>0)
+            {
+                int currentBottle = bottels.Pop();
+                int currentCup=cups.Peek();
+                if (currentBottle>=currentCup)
+                {
+                    wastedWater += currentBottle - currentCup;
+                    cups.Dequeue();
+                }
+                else
+                {
+                    while (bottels.Count>0)
+                    {
+                        currentCup -= currentBottle;
+                        currentBottle = bottels.Pop();
+                        if (currentCup-currentBottle<=0)
+                        {
+                            wastedWater += currentBottle - currentCup;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (cups.Count>0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
+            else
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottels)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+
+            }
+        }
+    }
+}
