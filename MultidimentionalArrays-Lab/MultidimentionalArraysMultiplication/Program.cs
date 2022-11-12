b5b6e10fa7eaa8b7b06fe5d54fb2861a9f943df6
Anastasiya.Ivanova <agb66666@gmail.com>
2022-11-12 16:29:05 +0200

    Added a solution.

---
+++ MultidimentionalArrays-Lab/MultidimentionalArraysMultiplication/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace MultidimentionalArraysMultiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[,] arrayOne = new int[n, m];
+            int[,] arrayTwo = new int[m, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < m; j++)
+                {
+                    arrayOne[i, j] = input[j];
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    arrayTwo[i, j] = input[j];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arrayOne[i, j] = arrayOne[i, j] * arrayTwo[j, i];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(arrayOne[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
